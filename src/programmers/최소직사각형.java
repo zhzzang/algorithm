562392bc75890d8571a2114d470c207a501db6be
zhzzang <leejh0799@gmail.com>
2023-01-11 09:15:19 +0900

    [Programmers] 최소직사각형 (Lv. 1, 완전탐색)

--- src/programmers/최소직사각형.java
+++ src/programmers/최소직사각형.java
@@ -3,16 +3,14 @@ package programmers;
 class Solution {
 
     public int solution(int[][] sizes) {
-        int[] larger = new int[sizes.length];
-        int[] smaller = new int[sizes.length];
         int maxWidth = 0;
         int maxHeight = 0;
 
         for (int i = 0; i < sizes.length; i++) {
-            larger[i] = Math.max(sizes[i][0], sizes[i][1]);
-            maxWidth = Math.max(maxWidth, larger[i]);
-            smaller[i] = Math.min(sizes[i][0], sizes[i][1]);
-            maxHeight = Math.max(maxHeight, smaller[i]);
+            int large = Math.max(sizes[i][0], sizes[i][1]);
+            maxWidth = Math.max(maxWidth, large);
+            int small = Math.min(sizes[i][0], sizes[i][1]);
+            maxHeight = Math.max(maxHeight, small);
         }
 
         return maxWidth * maxHeight;
